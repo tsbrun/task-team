efdfbd6ddac9ce4a2efb32a8e51333324ee29d0f
TSephanya Brun <tsbrun@icloud.com>
2021-09-16 16:27:44 -0400

    Configure devise form views values

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 class ApplicationController < ActionController::Base
     def index 
     end
+
+    helpers do
+        def resource_name
+            :user
+        end
+        
+        def resource_class 
+            User 
+        end
+        
+        def resource
+            @resource ||= User.new
+        end
+    end
 end
