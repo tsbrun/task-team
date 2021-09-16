b6fc42c1a64aa2910f74992a217e76a886d56af9
TSephanya Brun <tsbrun@icloud.com>
2021-09-16 15:58:30 -0400

    Permit sign up attributes in strong params

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,2 +1,8 @@
 class RegistrationsController < Devise::RegistrationsController
+
+    private 
+
+    def sign_up_params
+        params.require(:user).permit(:user_name, :first_name, :last_name, :password, :password_confirmation)
+    end
 end
