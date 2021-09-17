4c8ea723769bbc16990685839bb9e078302edb97
TSephanya Brun <tsbrun@icloud.com>
2021-09-17 13:47:31 -0400

    Minor formatting change

--- app/models/User.rb
+++ app/models/User.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  devise :omniauthable, omniauth_providers: %i[google_oauth2]
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: %i[google_oauth2]
 end
