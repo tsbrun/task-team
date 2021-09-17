735aa5ad0a08f868c64f79044baf40e2c1978e8f
TSephanya Brun <tsbrun@icloud.com>
2021-09-17 13:54:36 -0400

    Implement .from_omniauth in User model to create an instance of User from data provided by Google

--- app/models/User.rb
+++ app/models/User.rb
@@ -4,4 +4,10 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: %i[google_oauth2]
+
+  def self.from_omniauth(access_token)
+    data = access_token.info 
+    user = User.where(email: data['email']).first 
+    user 
+  end
 end
