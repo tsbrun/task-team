bb62d84cff13ec1ac11bc56b813696588d11e1d0
TSephanya Brun <tsbrun@icloud.com>
2021-09-17 13:46:55 -0400

    Handle google_oauth2 callbacks

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,2 +1,19 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+    skip_before_action :verify_authenticity_token, only: :google_oauth2
+
+    def google_oauth2
+        @user = User.from_omniauth(request.env["omniauth.auth"])
+
+        if @user.persisted? 
+            sign_in_and_redirect @user, event: :authentication # will throw if user not activated
+            set_flash_message(:notice, :success, kind: "Google") if is_navigational_format? 
+        else
+            session["devise.google_data"] = request.env["omniauth.auth"].except(:extra) # remove extra so session store doesn't overflow
+            redirect_to new_user_registration_url
+        end
+    end
+
+    def failure 
+        redirect_to root_path
+    end
 end
