028e6b01334f89b9a22920e2fabb409894248707
TSephanya Brun <tsbrun@icloud.com>
2021-09-24 14:29:38 -0400

    Generate Teams controller and new_team route/view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'teams/new', as: 'new_team'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "application#index"
   devise_for :users, :controllers => { registrations: 'registrations', omniauth_callbacks: 'users/omniauth_callbacks' }
