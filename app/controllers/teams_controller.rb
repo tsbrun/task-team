b0afc40e8218ce615fd17d689fa87f372937ac82
TSephanya Brun <tsbrun@icloud.com>
2021-09-24 14:52:58 -0400

    Instantiate new Team model for new_team form

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,4 +1,17 @@
 class TeamsController < ApplicationController
   def new
+    # new Team model for new_team form
+    @team = Team.new      
   end
+
+  #def create
+    #if saves => reroute to Team show page 
+    #else => reload new_team form with validation error 
+  #end
+
+  # private 
+
+  # def new_team_params 
+  #   params.require(:team).permit(:name, :goal)
+  # end
 end
