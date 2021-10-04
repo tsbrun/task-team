d65c3a6a02140691d61cb215baaccc1d14896263
TSephanya Brun <tsbrun@icloud.com>
2021-10-04 15:47:36 -0400

    Create List model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_24_181705) do
+ActiveRecord::Schema.define(version: 2021_10_04_194639) do
+
+  create_table "lists", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "teams", force: :cascade do |t|
     t.string "name"
