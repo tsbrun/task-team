394aa99dfc878d9fd4f3cabc48d845b424e8030a
TSephanya Brun <tsbrun@icloud.com>
2021-10-07 18:23:57 -0400

    Create new list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,7 +3,13 @@ class ListsController < ApplicationController
         @list = List.new
     end
 
-    # def create 
-    #     byebug
-    # end
+    def create
+        team = Team.find(params[:team_id])
+        @list = team.lists.build(title: params[:list][:title])
+        if @list.save 
+            redirect_to team_path(team)
+        else
+            render :new
+        end
+    end
 end
