b1aa30183b6e113828a51be8a1dfb5119282e175
TSephanya Brun <tsbrun@icloud.com>
2021-10-07 17:24:02 -0400

    Bug fix - correctly retrieve Google account info to successfully create instance of @user and enable Google Login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_07_203802) do
+ActiveRecord::Schema.define(version: 2021_10_07_211706) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
@@ -49,6 +49,8 @@ ActiveRecord::Schema.define(version: 2021_10_07_203802) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.integer "team_id"
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["team_id"], name: "index_users_on_team_id"
