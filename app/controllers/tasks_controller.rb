e2c3a00f175c5eaa1b4c86fc72862281b42026bc
TSephanya Brun <tsbrun@icloud.com>
2021-10-08 18:36:37 -0400

    Update task or render edit form

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,5 +4,19 @@ class TasksController < ApplicationController
     @team_members = @task.list.team.users
   end
 
-  
+  def update
+    @task = Task.find(params[:id])
+    # @user = User.find(params[:task][:user_id])
+    if @task.update(task_params)
+      redirect_to team_path(@task.list.team)
+    else
+      render :edit
+    end
+  end
+
+  params 
+
+  def task_params
+    params.require(:task).permit(:desc, :user_id)
+  end
 end
