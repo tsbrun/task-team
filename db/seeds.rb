85e9053e12ece65967989f344d77dcc6b6e5396b
TSephanya Brun <tsbrun@icloud.com>
2021-10-08 16:26:11 -0400

    Bug - editor and publisher instances not persisted to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,8 +12,9 @@ team = Team.create(name: 'Book Publishing Team', goal: "Publish Nom de Plume's n
 # Add members to the team
 team.users.build([
     {user_name: 'author101', first_name: 'Nom', last_name: 'de Plume', email: 'nomDePlume@yahoo.com', password: 'nomDePlume'},
-    {user_name: 'editor101', first_name: 'Ed', last_name: 'Tor', email: 'edTor@yahoo.com', password: 'edTor'},
-    {user_name: 'publisher101', first_name: 'Lisa', last_name: 'Agent', email: 'lizAgent', password: 'lizAgent'},
+    # AR mysteriously refuses to create editor and publisher
+    #{user_name: 'editor101', first_name: 'Ed', last_name: 'Tor', email: 'edTor@yahoo.com', password: 'edTor'},
+    #{user_name: 'publisher101', first_name: 'Lisa', last_name: 'Agent', email: 'lizAgent', password: 'lizAgent'},
     {user_name: 'manager101', first_name: 'Sosha', last_name: 'Mann', email: 'soshaMann@yahoo.com', password: 'soshaMann'}
 ]).each { |user| user.save }
 
