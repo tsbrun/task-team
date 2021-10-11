b46a32b571245681d3b403871a688f8ba152c430
TSephanya Brun <tsbrun@icloud.com>
2021-10-11 18:27:10 -0400

    Basic validations

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,6 @@
 class List < ApplicationRecord
+    validates :title, presence: true, uniqueness: true
+    
     has_many :tasks, dependent: :destroy
     has_many :users, through: :tasks
     belongs_to :team
